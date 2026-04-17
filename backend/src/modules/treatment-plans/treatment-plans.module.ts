5e74a50667669d4f9320f185481f8c995896bf2b
sodiero77-source <sodiero77@gmail.com>
2026-04-16 20:41:28 -0700

    feat: add treatment plans module wiring v1

--- backend/src/modules/treatment-plans/treatment-plans.module.ts
+++ backend/src/modules/treatment-plans/treatment-plans.module.ts
@@ -1,1 +1,12 @@
+import { Module } from '@nestjs/common';
+import { TreatmentPlansController } from './treatment-plans.controller';
+import { TreatmentPlansService } from './treatment-plans.service';
+import { PrismaModule } from '../../common/db/prisma.module';
 
+@Module({
+  imports: [PrismaModule],
+  controllers: [TreatmentPlansController],
+  providers: [TreatmentPlansService],
+  exports: [TreatmentPlansService],
+})
+export class TreatmentPlansModule {}
